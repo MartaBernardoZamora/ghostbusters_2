c7a2199f1139219fe4d9840eef5ffff0c4cd693c
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-11 15:37:30 +0100

    FantasmaModel unificado

---
+++ src/test/java/org/bibendum_coders/ghostbusters_2/controllers/FantasmaModel.java
@@ -0,0 +1,26 @@
+package org.bibendum_coders.ghostbusters_2.controllers;
+
+import java.time.LocalDateTime;
+
+public class FantasmaModel {
+    private String nombre;
+    private String clase;
+    private String nivelPeligro;
+    private String habilidad;
+    private LocalDateTime fechaCaptura;
+
+    public FantasmaModel(String nombre, String clase, String nivelPeligro, String habilidad) {
+        this.nombre = nombre;
+        this.clase = clase;
+        this.nivelPeligro = nivelPeligro;
+        this.habilidad = habilidad;
+        this.fechaCaptura = LocalDateTime.now();
+    }
+
+    public String getNombre() { return nombre; }
+    public String getClase() { return clase; }
+    public String getNivelPeligro() { return nivelPeligro; }
+    public String getHabilidad() { return habilidad; }
+    public LocalDateTime getFechaCaptura() { return fechaCaptura; }
+
+}
