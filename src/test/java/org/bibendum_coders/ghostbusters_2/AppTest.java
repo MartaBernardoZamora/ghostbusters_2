ea1d4688157fd19354ad74f3ae211d662e7dc347
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-11 14:50:08 +0100

    initial commit

---
+++ src/test/java/org/bibendum_coders/ghostbusters_2/AppTest.java
@@ -0,0 +1,12 @@
+package org.bibendum_coders.ghostbusters_2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
