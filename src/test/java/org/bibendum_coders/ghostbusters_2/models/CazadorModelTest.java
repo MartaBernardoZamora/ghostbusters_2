4765be7b1875e05455d28d1795343bd188b54893
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-12 20:46:29 +0100

    TestCazadorModel unido y status ok 100%

--- src/test/java/org/bibendum_coders/ghostbusters_2/models/CazadorModelTest.java
+++ src/test/java/org/bibendum_coders/ghostbusters_2/models/CazadorModelTest.java
@@ -1,25 +1,58 @@
 package org.bibendum_coders.ghostbusters_2.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+
 public class CazadorModelTest {
+    private CazadorModel cazador;
+
+    @BeforeEach
+    public void setUp() {
+        cazador = new CazadorModel();
+    }
+    
     @Test
     void testAddFantasma() {
-
+        FantasmaModel fantasmaModel = new FantasmaModel(1, "fantasmico", Clase.CLASE4, "medio", "vuela");
+        cazador.addFantasma(fantasmaModel);
+        List<FantasmaModel> result = cazador.getFantasmas();
+        assertThat(result.size(), is(1));
     }
 
     @Test
     void testDeleteFantasma() {
-
+        FantasmaModel fantasmaModel = new FantasmaModel(1, "fantasmico", Clase.CLASE4, "medio", "vuela");
+        cazador.addFantasma(fantasmaModel);
+        int fantasmaNumber = 1; /*Numero proporcionado por el usuario*/
+        cazador.deleteFantasma(fantasmaNumber);
+        List<FantasmaModel> result = cazador.getFantasmas();
+        assertThat(result.size(), is(0));
     }
 
     @Test
     void testGetContadorId() {
+        int result = cazador.getContadorId();
+        assertThat(result, is(1));
 
     }
 
     @Test
     void testGetFantasmas() {
+        FantasmaModel fantasmaModel = new FantasmaModel(1, "fantasmico", Clase.CLASE4, "medio", "vuela");
+        List<FantasmaModel> result = cazador.getFantasmas();
+        List<FantasmaModel> resultExpected = new ArrayList<>();
+        assertThat(result, is(resultExpected));
+        cazador.addFantasma(fantasmaModel);
+        result = cazador.getFantasmas();
+        assertThat(result.size(), is(1));
+        assertTrue(result.contains(fantasmaModel));
 
     }
 }
