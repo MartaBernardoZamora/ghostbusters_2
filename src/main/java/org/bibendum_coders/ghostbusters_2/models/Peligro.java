c52462b5aa0e92efed9bb0371ce63db161fe613e
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-16 15:25:53 +0100

    Status ok Test getPeligroString and enum Peligro creado

--- src/main/java/org/bibendum_coders/ghostbusters_2/models/Peligro.java
+++ src/main/java/org/bibendum_coders/ghostbusters_2/models/Peligro.java
@@ -1,5 +1,18 @@
 package org.bibendum_coders.ghostbusters_2.models;
 
 public enum Peligro {
+    BAJO("BAJO"),
+    MEDIO("MEDIO"),
+    ALTO("ALTO");
+    
+    private final String peligroString;
 
+    Peligro(String peligroString) {
+        this.peligroString = peligroString;
+    }
+
+    
+    public String getPeligroString() {
+        return peligroString;
+    }
 }
